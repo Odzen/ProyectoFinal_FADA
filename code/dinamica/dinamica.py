3db8e48efd873ee65d2b9b77b44004ca2c9ad2ab
Juan Sebastian Velasquez <jsebastian.va@gmail.com>
2022-03-14 03:54:06 -0500

    Cambie a -> 0 y b -> 1, porque de esta forma sera mas facil moverme a traves de L para encontrar la solucion optima

--- code/dinamica/dinamica.py
+++ code/dinamica/dinamica.py
@@ -41,15 +41,15 @@ def llenarMatrices(n, a, b, ab, ba):
     
     # Array para guardar la matriz auxiliar de la solucion optima, de la cual
     # luego se calcula la solucion optima
-    L = np.empty([2, n], dtype = str)
+    L = np.zeros([2, n], dtype = int)
 
     # Casos base, cuando se analiza la actividad 1
     # Recordar que por facilidad, indexamos empezando 0
     T[0,0] = a[0]
     T[1,0] = b[0]
     
-    L[0,0] = 'a'
-    L[1,0] = 'b'
+    L[0,0] = 0
+    L[1,0] = 1
 
     # Ciclo for de 1 hasta n sin incluirlo, iterando de a 1
     # Populando matriz de valor de solucion optima y matriz auxiliar
@@ -57,18 +57,18 @@ def llenarMatrices(n, a, b, ab, ba):
         # Computar los valores de T[0,j] y L[0,j], fila 1 de cada array
         if T[0,j-1] +  a[j] <= T[1, j-1] + ba[j-1] + a[j]:
             T[0,j] = T[0, j-1] + a[j]
-            L[0,j] = 'a'
+            L[0,j] = 0
         else:
             T[0,j] = T[1, j-1] + ba[j-1] + a[j]
-            L[0,j] = 'b'
+            L[0,j] = 1
         
         # Computar los valores de T[1,j] y L[1,j], fila 2 de cada array
         if T[1,j-1] +  b[j] <= T[0, j-1] + ab[j-1] + b[j]:
             T[1,j] = T[1,j-1] +  b[j]
-            L[1,j] = 'b'
+            L[1,j] = 1
         else:
             T[1,j] = T[0, j-1] + ab[j-1] + b[j]
-            L[1,j] = 'a'
+            L[1,j] = 0
 
     return T,L
 
@@ -87,10 +87,10 @@ def solucionesOptimas(T,L,n):
     # solucion optima
     if(T[0,n-1] <= T[1,n-1]):
         time += T[0,n-1]
-        finalLine='a'
+        finalLine=0
     else:
         time += T[1,n-1]
-        finalLine='b'
+        finalLine=1
     
     return time,finalLine
     
